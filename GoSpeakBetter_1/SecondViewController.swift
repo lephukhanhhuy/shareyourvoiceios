1c41b35f07e827aef59d238aa18957875cab2963
Your Name <you@example.com>
2015-08-01 10:42:20 +0700

    Initial Commit

---
+++ GoSpeakBetter_1/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  GoSpeakBetter_1
+//
+//  Created by Le Huy on 8/1/15.
+//  Copyright (c) 2015 Huy Le. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
